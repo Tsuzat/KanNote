41408d1593fe5ea05e18cdc76540744379e9f11d
Tsuzat <dev.tsuzat@gmail.com>
2024-12-03 22:33:30 +0530

    add: Modewatcher, sonner and css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import '@fontsource-variable/dm-sans';
 	import '../app.css';
+	import Sonner from '$lib/components/ui/sonner/sonner.svelte';
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
+<Sonner />
 
 {@render children()}
